598e9c5ce91c92c399393db23f74104b581a7d43
Meori Lehr <Meori Lehr@DESKTOP-IMHE072>
2016-09-28 08:25:02 -0700

    adding admin authentication and authorization

--- FinalProject/Controllers/AdminController.cs
+++ FinalProject/Controllers/AdminController.cs
@@ -1,17 +1,48 @@
-﻿using System;
+﻿using FinalProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace FinalProject.Controllers
 {
     public class AdminController : Controller
     {
         // GET: Admin
-        public ActionResult Index()
+        public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginData model)
+        {
+            // Validate model
+            if (ModelState.IsValid)
+            {
+                // Check if the admin password is entered correctly
+                if (model.UserName.ToLower() == "admin" && model.Password == "Aa123456")
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The user login or password provided is incorrect.");
+                }
+            }
+            
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+
+            return RedirectToAction("Login", "Admin");
         }
     }
 }
